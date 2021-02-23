d8711c7e229cf73e2502255f2a704a994c3dc629
sugarfixx <ingar.torsrud@me.com>
2021-02-23 07:31:19 +0100

    Ad item builder

---
+++ builder/src/ItemBuilder.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace ItemBuilder;
+
+
+class ItemBuilder implements ItemBuilderInterface
+{
+    public function setTitle($title)
+    {
+        // TODO: Implement setTitle() method.
+    }
+
+    public function getItem()
+    {
+        // TODO: Implement getItem() method.
+    }
+
+    public function setItem($item)
+    {
+        // TODO: Implement setItem() method.
+    }
+
+
+}
