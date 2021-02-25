64f52d8a3f2c8bb7f7cc0ef74fcef0eba76f7816
sugarfixx <ingar.torsrud@me.com>
2021-02-25 09:21:37 +0100

    Implement XML formatter

--- adapter/src/XMLFormatter.php
+++ adapter/src/XMLFormatter.php
@@ -8,6 +8,20 @@ class XMLFormatter implements Formatter
 {
     public function render($data)
     {
-        // TODO: Implement render() method.
+        header("Content-type: text/xml");
+        echo $this->transform($data);
+    }
+
+    public function transform($data)
+    {
+        $d = json_encode($data);
+        $array = json_decode($d, true);
+        $xml = new SimpleXMLElement('<products/>');
+        foreach ($array as $item) {
+            $product = $xml->addChild('product');
+            $product->addChild('title', $item['title']);
+            $product->addChild('type', $item['type']);
+        }
+        return $xml->asXML();
     }
 }
