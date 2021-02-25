953f4368c76bcc9b89da1d53ada2f037084b83b6
sugarfixx <ingar.torsrud@me.com>
2021-02-25 09:20:09 +0100

    Add XML andf JSON formatter

---
+++ adapter/src/JSONFormatter.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Adapter;
+
+
+class JSONFormatter implements Formatter
+{
+    public function render($data)
+    {
+        // TODO: Implement render() method.
+    }
+}
---
+++ adapter/src/XMLFormatter.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Adapter;
+
+
+class XMLFormatter implements Formatter
+{
+    public function render($data)
+    {
+        // TODO: Implement render() method.
+    }
+}
