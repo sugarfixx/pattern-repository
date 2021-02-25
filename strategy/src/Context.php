daabf1bc8a4316ce0a922c4e922484be74a351a9
sugarfixx <ingar.torsrud@me.com>
2021-02-25 18:46:17 +0100

    Implements context

--- strategy/src/Context.php
+++ strategy/src/Context.php
@@ -3,5 +3,31 @@
 
 class Context
 {
+    private $strategy;
 
+    public function __construct(Strategy $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
+    public function setStrategy(Strategy $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
+    public function doSomeStuff(): void
+    {
+        $products = [
+            ['type' => 'book', 'title' => 'Real Life'],
+            ['type' => 'video', 'title' => 'Bohemian Rhapsody'],
+            ['type' => 'video', 'title' => 'Green Book'],
+            ['type' => 'audio', 'title' => 'Plastic Hearts'],
+            ['type' => 'book', 'title' => 'Future Nostalgia'],
+            ['type' => '', 'title' => 'Whoopps! what is this?'],
+        ];
+
+        $result = $this->strategy->doOrder($products);
+        header("Content-type: application/json");
+        echo json_encode($result, JSON_PRETTY_PRINT);
+    }
 }
