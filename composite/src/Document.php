24a6c931531dc20a59762df0618c829cf13019c1
sugarfixx <ingar.torsrud@me.com>
2021-04-12 08:36:58 +0200

    Add Property and refactir Document

--- composite/src/Document.php
+++ composite/src/Document.php
@@ -9,7 +9,39 @@
 namespace Composite;
 
 
-abstract class Document
+class Document extends Field
 {
-    protected $parent;
+    protected $children;
+
+    public function __construct()
+    {
+        $this->children = new \SplObjectStorage();
+    }
+
+    public function add(Field $component): void
+    {
+        $this->children->attach($component);
+        $component->setParent($this);
+    }
+
+    public function remove(Field $component): void
+    {
+        $this->children->detach($component);
+        $component->setParent(null);
+    }
+
+    public function isComposite(): bool
+    {
+        return true;
+    }
+
+    public function operation(): string
+    {
+        $results = [];
+        foreach ($this->children as $child) {
+            $results[] = $child->operation();
+        }
+
+        return "Branch(" . implode("+", $results) . ")";
+    }
 }
---
+++ composite/src/Property.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 12/04/2021
+ * Time: 08:01
+ */
+
+namespace Composite;
+
+
+class Property extends Field
+{
+    public function operation(): string
+    {
+        return "Property";
+    }
+}
